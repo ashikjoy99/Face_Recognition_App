8474e5f9085ac9b033fa3fb710afad6ded5a16d5
Ashik Joy <79218049+ashikjoy99@users.noreply.github.com>
2021-05-22 19:15:01 +0530

    Updates

---
+++ precache-manifest.98b9d730c989eb19d2b3846c86565b68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8212638677682a817e7c47279362a083",
+    "url": "/Face_Recognition_App/index.html"
+  },
+  {
+    "revision": "1875e9bd2536bf288c7b",
+    "url": "/Face_Recognition_App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cab86a82864a5b7ea1a0",
+    "url": "/Face_Recognition_App/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "1875e9bd2536bf288c7b",
+    "url": "/Face_Recognition_App/static/js/2.ed30709e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Face_Recognition_App/static/js/2.ed30709e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab86a82864a5b7ea1a0",
+    "url": "/Face_Recognition_App/static/js/main.db5f6989.chunk.js"
+  },
+  {
+    "revision": "4767d738a4fdc10ad9f4",
+    "url": "/Face_Recognition_App/static/js/runtime-main.cb8627cf.js"
+  }
+]);
